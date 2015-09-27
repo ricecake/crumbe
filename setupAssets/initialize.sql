dd9267172878856ce9c137521b50b2ffb884230a
Sebastian Green-Husted <ricecake@tfm.nu>
2015-09-27 15:18:47 -0500

    Create initialize.sql

---
+++ setupAssets/initialize.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+CREATE TABLE post (
+);
+CREATE TABLE body (
+);
+CREATE TABLE comment (
+);
+CREATE TABLE style (
+);
+CREATE TABLE script (
+);
+CREATE TABLE link (
+);
+CREATE TABLE user (
+);
+
+COMMIT;
