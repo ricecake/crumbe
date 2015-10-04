ed772b822fcc247f09c6c716b08358bb4ea6e30d
Sebastian Green-Husted <ricecake@tfm.nu>
2015-10-04 18:42:13 -0400

    Update crumbe_content.erl

--- apps/crumbe/src/crumbe_content.erl
+++ apps/crumbe/src/crumbe_content.erl
@@ -1,2 +1,10 @@
 -module(crumbe_post).
 -export([render/1, preview/1, comment/1, post/1]).
+
+render(Id) -> {ok, <<>>}.
+
+preview(Id) -> {ok, <<>>}.
+
+comment(Id) -> {ok, <<>>}.
+
+post(Id) -> {ok, <<>>}.
